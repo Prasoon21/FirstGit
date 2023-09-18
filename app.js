c5e3084dba281a86879b8afbdd66426764496b86
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-09-18 12:11:05 +0530

    worked on tagName

--- app.js
+++ app.js
@@ -29,16 +29,53 @@
 
 //GETELEMENTSBYCLASSNAME
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'bye bye';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
-items[2].style.backgroundColor = 'green';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'bye bye';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+// items[2].style.backgroundColor = 'green';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+//GETELEMENTSBYTAGNAME
+
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'bye bye';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+// li[2].style.backgroundColor = 'green';
+
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight = 'bold';
+// }
+
+// var li = document.getElementsByClassName('new-list');
+
+// console.log(li);
+// li[0].style.backgroundColor = 'blue';
+
+var li = document.getElementsByTagName('li');
+
+console.log(li);
+for(var j=0;j<li.length;j++){
+    li[j].style.backgroundColor = 'blue';
 }
+
+
+
+// QUERYSELECTOR
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'hello world';
+
 
 
