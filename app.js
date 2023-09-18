2aa1ae60c0cc07ea1a0882bcdf2dcbc1eb3238fe
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-09-18 11:24:39 +0530

    manipulating DOM

--- app.js
+++ app.js
@@ -29,12 +29,16 @@
 
 //GETELEMENTSBYCLASSNAME
 
-var items = document.getElementsByClassName('title');
+var items = document.getElementsByClassName('list-group-item');
 console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'bye bye';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
-items[0].style.fontWeight = 'bold';
-items[0].style.color = 'green';
+console.log(items[1]);
+items[1].textContent = 'bye bye';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+items[2].style.backgroundColor = 'green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
+
 
