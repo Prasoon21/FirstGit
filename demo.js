99ac2f481a88dd69bccca325839b8e8577abc474
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-09-19 22:55:32 +0530

    deletinon and additon of elements

--- demo.js
+++ demo.js
@@ -1,1 +1,50 @@
-console.log('new file adding and then displaying')
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+//form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+// ADD item
+function addItem(e){
+    e.preventDefault();
+    
+    // Get input value
+    var newItem = document.getElementById('item').value;
+
+    // Create new li element
+    var li = document.createElement('li');
+    //Add class
+    li.className = 'list-group-item';
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    // create delete button li
+    var deleteButton = document.createElement('button');
+    var editBtn = document.createElement('button');
+
+    // Add classes to deleteButton
+    deleteButton.className = 'btn btn-danger btn-sm float-right delete';
+    editBtn.className = 'btn btn-light btn-sm float-right edit';
+
+    //append text node
+    deleteButton.appendChild(document.createTextNode('X'));
+    editBtn.appendChild(document.createTextNode('edit'));
+
+    //append button to li
+    li.appendChild(deleteButton);
+    li.appendChild(editBtn);
+
+    itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
