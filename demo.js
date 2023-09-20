6c5b53d5aebbd59a65858ad1ac75a51d294c728c
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-09-20 10:00:47 +0530

    workde with filters

--- demo.js
+++ demo.js
@@ -1,10 +1,13 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 //form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter Event
+filter.addEventListener('keyup', filterItems);
 
 // ADD item
 function addItem(e){
@@ -12,6 +15,8 @@ function addItem(e){
     
     // Get input value
     var newItem = document.getElementById('item').value;
+    var itemDescription = document.getElementById('description').value;
+    var mybr = document.createElement('br');
 
     // Create new li element
     var li = document.createElement('li');
@@ -19,6 +24,9 @@ function addItem(e){
     li.className = 'list-group-item';
     // Add text node with input value
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(mybr);
+    // adding description with items
+    li.appendChild(document.createTextNode(" " + itemDescription));
 
     // create delete button li
     var deleteButton = document.createElement('button');
@@ -37,6 +45,10 @@ function addItem(e){
     li.appendChild(editBtn);
 
     itemList.appendChild(li);
+
+    form.reset();
+    // newItem.value = '';
+    // itemDescription.value = '';
 }
 
 // Remove item
@@ -47,4 +59,24 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+// filter items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // get lis
+    var items = itemList.getElementsByTagName('li');
+    // convert to array
+    Array.from(items).forEach(function(item) {
+        var itemName = item.firstChild.textContent;
+        var des = item.firstChild.nextSibling.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 || des.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+
+        }else{
+            item.style.display = 'none';
+        }
+        
+    });
 }
